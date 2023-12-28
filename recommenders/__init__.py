3fe139c3d0502c0607ca333f4274dc2e97bbbcc5
frostbite127 <141996530+frostbite127@users.noreply.github.com>
2023-12-28 20:16:52 +0530

    Update __init__.py

--- recommenders/__init__.py
+++ recommenders/__init__.py
@@ -5,9 +5,23 @@ import os
 #print(os.getcwd())
 
 default_poster_url = "https://gurukul-be.s3.ap-south-1.amazonaws.com/0c3df9f4dc56471d88bb88255bb79548.jpg"
-data_ratings = pd.read_csv('data_files/ratings.csv')
-data_movies = pd.read_csv('data_files/movies.csv')
-data_posters = pd.read_csv('data_files/movie_poster.csv')
 
+####################################################################################################################################
+
+'''
+Complete the code below
+
+Things to do: load csv in dataframes
+
+data_ratings => file path : 'data_files/ratings.csv'
+data_movies => file path : 'data_files/movies.csv'
+data_posters => file path : 'data_files/movie_poster.csv'
+
+'''
+data_ratings = None
+data_movies = None
+data_posters = None
+
+#####################################################################################################################################
 data_movies = pd.merge(data_movies, data_posters, how="left", on="movieId")
 
